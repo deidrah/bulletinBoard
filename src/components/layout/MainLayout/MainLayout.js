76fe0d1bfdc76ce03fae8c50897a125b0c842870
Deidrah <dominikastasik93@gmail.com>
2021-09-11 09:10:27 +0200

    add header component

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -7,11 +7,14 @@ import clsx from 'clsx';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './MainLayout.module.scss';
+import { Header } from "../Header/Header";
 
 const Component = ({className, children}) => (
   <div className={clsx(className, styles.root)}>
-    <h2>MainLayout</h2>
-    {children}
+    <div className={styles.container}>
+      <Header />
+      {children}
+    </div>
   </div>
 );
 
