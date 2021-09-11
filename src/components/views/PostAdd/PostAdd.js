46d4bc7a237be96a77f541512c401cef8b797761
Deidrah <dominikastasik93@gmail.com>
2021-09-11 10:27:17 +0200

    add PostAdd component and function

--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -1,37 +1,131 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import clsx from 'clsx';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import CardHeader from '@material-ui/core/CardHeader';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import clsx from 'clsx';
+import shortid from 'shortid';
+
+import { connect } from 'react-redux';
+import { getAll, addPost } from '../../../redux/postsRedux.js';
 
 import styles from './PostAdd.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>PostAdd</h2>
-    {children}
-  </div>
-);
+const Component = ({ className, addPost }) => {
 
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
+  const today = new Date();
+  const day = today.getDate();
+  const month = today.getMonth();
+  const year = today.getFullYear();
+  const date = day + '/' + month + '/' + year;
+
+  const titleProps = {
+    minLength: 10,
+  };
+
+  const contentProps = {
+    minLength: 20,
+  };
+
+  const [post, setPost] = React.useState({
+    id: shortid.generate(),
+    date: date,
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log('Submitted');
+    await addPost(post);
+  };
+
+  const handleChange = async (event, name) => {
+    await setPost({
+      ...post,
+      [name]: event.target.value,
+    });
+  };
+
+  // console.log('post', post);
+
+  return (
+    <div className={clsx(className, styles.root)}>
+      <Container maxWidth="lg">
+        <Card className={styles.card}>
+          <CardHeader title="Add new post" />
+          <form className={styles.form} autoComplete="off" onSubmit={e => handleSubmit(e)}>
+            <TextField
+              id="title"
+              label="Title"
+              required
+              inputProps={titleProps}
+              value={post.title}
+              onChange={e => handleChange(e, 'title')}
+            />
+            <TextField
+              id="price"
+              label="Price"
+              type="number"
+              value={post.price}
+              onChange={e => handleChange(e, 'price')}
+            />
+            <TextField
+              variant="outlined"
+              multiline
+              id="content"
+              inputProps={contentProps}
+              label="Content"
+              placeholder="Write your post here"
+              rows="10"
+              required
+              value={post.content}
+              onChange={e => handleChange(e, 'content')}
+
+            />
+            <TextField
+              id="mail"
+              label="E-mail"
+              type="email"
+              required
+              value={post.mail}
+              onChange={e => handleChange(e, 'mail')}
+            />
+            <TextField
+              id="phone"
+              label="Phone number"
+              type="number"
+              value={post.phone}
+              onChange={e => handleChange(e, 'phone')}
+            />
+            <Button type="submit" color="secondary" variant="contained" className={styles.button}>Save</Button>
+            <Button color="secondary" href="/" variant="contained" className={styles.button}>Return</Button>
+          </form>
+        </Card>
+      </Container>
+    </div>
+  );
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+Component.propTypes = {
+  className: PropTypes.string,
+  addPost: PropTypes.func,
+};
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapStateToProps = state => ({
+  posts: getAll(state),
+});
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const mapDispatchToProps = dispatch => ({
+  addPost: post => dispatch(addPost(post)),
+});
+
+const PostAddContainer = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  Component as PostAdd,
-  // Container as PostAdd,
+  // Component as PostAdd,
+  PostAddContainer as PostAdd,
   Component as PostAddComponent,
 };
